9344da10cc4017ced984ea10b84eb2cd10b28c99
Esteve Millán <esteve@MacBook-Pro-de-Esteve.local>
2019-01-08 13:15:09 +0100

    db challanege update2

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -4,11 +4,9 @@ import MovieDetail from '../../components/movieList/MovieDetail';
 
 class Detail extends Component {
 
-  
   render(){
     const {movie} = this.props.location.state.movieInfo;
     return (
-      
       <div>
         <Link to='/'> llevame a home </Link>
         <MovieDetail movieInfo={movie}/>
